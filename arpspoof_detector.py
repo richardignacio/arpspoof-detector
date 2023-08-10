34b64c9b3752b19ef47594a85d84d8d16c7f69c0
Richard Ignacio <richard.ignacio@protonmail.com>
2023-08-10 11:38:48 -0700

    Updated with code and comments from last class

--- arpspoof_detector.py
+++ arpspoof_detector.py
@@ -1,61 +1,96 @@
 """
-arpspoof_detector.py
-
-This program monitors the host's arp cache for
-duplicate MAC addresses
+ARP Spoof Detector - Final Project
+    Tool to check if an ARP spoofing attack is in progress
 """
-import os
-import sys
 import datetime
-from time import sleep
+import os
+import time
 
-LOG_FILE = 'duplicates.log'
+""" Global Variables """
+
+LOG_FILE = 'arpspoof.log'
+
+""" Functions """
 
 
 def extract_arp_table():
-    arp_table = {}
-    raw_arp_table = os.popen("arp -a").read()
-    table_lines = raw_arp_table.split("\n")
-    for line in table_lines:
-        line_items = line.split()
-        if "dynamic" in line_items:
-            ip = line_items[0]
-            mac = line_items[1]
-            arp_table[ip] = mac
-    return arp_table
+    """ Project Task 1: ARP Table Extraction
+        1. Execute the "arp -a" command
+        2. Capture the output and store variable
+        3. Parse the output in the variable
+        4. Store the IP address to MAC mapping into a dictionary
+    """
+
+    # Dictionary to hold the IP-to-MAC mapping
+    ip_mac_map = {}
+
+    # Execute the system command and store it in a variable
+    command_output = os.popen("arp -a").read()
+    # Split the output of the command into separate lines
+    output_lines = command_output.splitlines()
+
+    # Process each line in the command's output
+    for line in output_lines:
+        # Split the line by whitespace
+        line_split = line.split()
+
+        # Process only the entries in the list that contain a 'dynamic' type
+        if 'dynamic' in line_split:
+            # Pull out the key fields by referring to the corresponding index
+            ip_address = line_split[0]
+            mac_address = line_split[1]
+
+            # Add the IP Address to MAC mapping to the dictionary
+            ip_mac_map[ip_address] = mac_address
+
+    return ip_mac_map
 
 
-def log_duplicates(ip, mac):
-    with open(LOG_FILE, "a") as log_file:
-        timestamp = datetime.datetime.now()
-        entry = f"{timestamp} arpspoof_detector: Duplicate MAC found: {mac} with IP {ip}"
-        log_file.write(entry)
+def id_duplicate_macs(arp_table):
+    """ Project Task 2: Identifying MAC Address Duplication """
 
-
-def detect_duplicate_macs(arp_table):
+    duplicate = None
     macs_seen = []
-    for ip in arp_table:
+
+    for ip in arp_table.keys():
         mac = arp_table[ip]
         if mac in macs_seen:
-            print(f"Found duplicate {mac} with {ip}")
-            log_duplicates(ip, mac)
+            print(f"*** Found a duplicate MAC: {mac}")
+            duplicate = mac
+            break
         else:
             macs_seen.append(mac)
+    return duplicate
 
 
-if __name__ == "__main__":
-    while True:
-        try:
-            print("Detecting...")
-            host_arp_table = extract_arp_table()
-            detect_duplicate_macs(host_arp_table)
-            sleep(5)
-        except KeyboardInterrupt:
-            print("Exiting program")
-            sys.exit(1)
+def log_entry(message):
+    """ Project Task 3: Logging Events """
+
+    timestamp = datetime.datetime.now()  # Format looks like: 2023-04-23 14:28:37.287318
+
+    with open(LOG_FILE, 'a') as arp_log:
+        arp_log.write(f"{timestamp} - arpspoof.py: {message}\n")
 
 
+def main():
+    try:
+        # Use an infinite loop to continuously check for an attack
+        while True:
+            print("Detecting arpspoof attacks...")
+            arp_map = extract_arp_table()
+            duplicate_mac = id_duplicate_macs(arp_map)
+            if duplicate_mac:
+                log_entry(f"Duplicate MAC address found: {duplicate_mac}")
+            time.sleep(1)
 
+    # Catch Ctrl-C or the stop button in the IDE
+    except KeyboardInterrupt:
+        print("Stopping arpspoof attack detection")
+
+
+# Check if script is directly executed or is being imported
+if __name__ == '__main__':
+    main()
 
 
 
